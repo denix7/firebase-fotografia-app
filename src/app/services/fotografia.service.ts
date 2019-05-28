0c6802948bac578b7b686003bcc66530f29e6a81
Denix7 <dennis.brian.pv@gmail.com>
2019-05-28 11:43:50 -0400

    Creando componente de perfil y servicio de fotografia

--- src/app/services/fotografia.service.ts
+++ src/app/services/fotografia.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from "@angular/common/http";
 })
 export class FotografiaService {
 
-  urlFotos:string = "https://tris-portafolio.firebaseio.com/fotografia.json";
+  urlFotos:string = "https://tris-portafolio-5162e.firebaseio.com/fotografias.json";
   
   constructor(public _http: HttpClient) { }
     
